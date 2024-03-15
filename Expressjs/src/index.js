60248bdf7b3c7a94b1b8a536ad3884ad4294e9d4
princi <princisharma086@gmail.com>
2024-03-16 00:28:50 +0530

    learn new concept

--- Expressjs/src/index.js
+++ Expressjs/src/index.js
@@ -1,24 +1,88 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser')
 
-const path=require("path");
-const express=require("express")
-const app=express();
+const static = path.join(__dirname, "../public");
 
-const static=path.join(__dirname, "../public" );
+app.set("view engine", "ejs"); 
+app.use(bodyParser.urlencoded({extended: true}))
+// app.use(cors())
+
+let login  = false;
+const auth = function(req, res, next){
+  if(login){
+    next()
+  }else{
+    res.redirect('/login')
+    next()
+  }
+}
+app.use(auth)
 
 
-app.use(express.static(static))
+// app.use(express.static(static));
 
-app.get("/",(req,res)=>{
-    res.send("this is the home page");
+app.get("/", (req, res) => {
+  
+  res.render("index1");
+});
+
+app.get('/login', (req, res) => {
+  login = true;
+  res.send("Login Page");
 })
-app.get("/about",(req,res)=>{
-    res.send("this is the about  page");
+
+app.post("/add", (req, res) => {
+  console.log(req.body)
+  let name = req.body.name
+  res.send("Data: ", name)
 })
 
-app.get("/test", ()=>{
-    res.send("Testing Route")
+
+app.get("/logout", (req, res) => {
+  login = false;
+  res.send("this is the about  page");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Testing Route from express app!");
+});
+
+app.listen(8000, () => {
+  console.log("listeing");
+});
+
+console.log("hello primcy");
+
+
+/*
+Middleware: 
+            pdf file/ Web page -> 
+*/
+
+/* 
+ php -> get post
+Request Method
+Get Post patch put delete
+
+/users get
+/users post
+/users patch
+/users put
+/users delete
+
+{
+  id: 3,
+  method: "delete/ update"
+}
+
+app.post(()=>{
+  if (method == 'delete'){
+
+  }else if(method == 'update'){
+    
+  }
 })
 
-app.listen(8000,()=>{
-    console.log("listeing");
-})
+*/
