f06575ace58e300e1b7e1e279d7197eb5542b62f
princi <princisharma086@gmail.com>
2024-03-17 17:24:27 +0530

    learn partials in express

---
+++ Expressjs/src/login.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+
+// const loginpath=path.join(__dirname,"/template/views")
+// consoleso
+
+// app.set("views",loginpath);
+
+const partialpath = path.join(__dirname, "./template/partials");
+
+
+app.set("view engine", "hbs");
+app.get("/", (req, res) => {
+  res.render("home");
+});
+hbs.registerPartials(partialpath);
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+app.get("/about", (req, res) => {
+  res.send("this is the about page");
+});
+app.listen(8000, () => {
+  console.log("8000 port is listing now");
+});
