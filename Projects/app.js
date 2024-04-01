c23d4390b0eb388fdce7d1322c849ff4f2eab48f
princi <princisharma086@gmail.com>
2024-04-01 12:32:52 +0530

    work ongoing

--- Projects/app.js
+++ Projects/app.js
@@ -8,6 +8,12 @@ app.set("view engine","hbs")
 app.get("/home",(req,res)=>[
     res.render("index")
 ])
+app.get("/register",(req,res)=>{
+    res.render("register");
+})
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
 app.listen(port,()=>{
     console.log("connect to the server");
 })
