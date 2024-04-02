52a6baea4bd0554462b5d96ee3c6cf410089f992
princi <princisharma086@gmail.com>
2024-04-03 00:10:56 +0530

    complete the login form

--- Projects/app.js
+++ Projects/app.js
@@ -1,19 +1,82 @@
 const express=require("express");
 const app=express();
 const port=process.env.PORT || 8000;
+const path=require("path");
+const hbs=require("hbs");
+require("./src/db/conn");
+const register=require("./src/model/model");
 
-
-
+const templatepath=path.join(__dirname,"./templates/views");
+console.log(__dirname);
+const partial_path=path.join(__dirname,"./templates/partials");
+app.set("views",templatepath)
 app.set("view engine","hbs")
-app.get("/home",(req,res)=>[
+hbs.registerPartials(partial_path);
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+
+app.get("/home",(req,res)=>{
     res.render("index")
-])
+
+})
 app.get("/register",(req,res)=>{
     res.render("register");
+})
+
+app.post("/register",async (req,res)=>{
+   try{
+    const password=req.body.password;
+    const cpassword=req.body.confirmpass;
+    if(password===cpassword){
+
+        const data=new register({
+            firstname:req.body.firstname,
+            lastname:req.body.lastname,
+            Email:req.body.email,
+            password:req.body.password,
+            Confirmpassword:req.body.confirmpass,
+        })
+       const datastore=await data.save();
+       res.status(201).send(datastore);
+
+    }
+    else{
+        res.send("password does not match");
+    }
+
+
+   }
+   catch(e){
+    res.status(400).send(e);
+   }
 })
 app.get("/login",(req,res)=>{
     res.render("login")
 })
+
+app.post("/login",async (req,res)=>{
+    try{
+
+        const email=req.body.email;
+        const useremail=await  register.findOne({Email:email});
+        const password=req.body.password;
+        
+        if(password===useremail.password){
+            res.render("index")
+        }
+        else{
+            res.send("invalid login destails:")
+        }
+
+
+
+    }
+    catch(e){
+        res.status(400).send(e);
+    }
+})
+
 app.listen(port,()=>{
     console.log("connect to the server");
 })
